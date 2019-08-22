6e5cabb9c71f9effd7bb9238fb658f0c3d8d1f21
Shuhei Hayakawa <hayashu@post.j-parc.jp>
2019-08-22 22:44:36 +0900

    minor update

--- shell/run.sh
+++ shell/run.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-hyptpc1 param/conf/default.conf tmp.root g4macro/run.mac
+conf=param/conf/default.conf
+root=tmp.root
+macro=g4macro/run.mac
+log=log/tmp.log
+mkdir -p log
+
+case $HOSTNAME in
+    *.cc.kek.jp)
+	bsub -q s -o $log hyptpc1 $conf $root $macro
+	;;
+    *)
+	echo "hyptpc1 $conf $root $macro >$log 2>&1" | batch
+	;;
+esac
