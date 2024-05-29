59207ed3d69e64d846ddd59abbf6fbe217de64f3
KANG Byung Min <kangbm@ccw01.cc.kek.jp>
2024-05-29 13:11:23 +0900

    Resolution scale parameter set to default.

--- src/ResHypTPC.cc
+++ src/ResHypTPC.cc
@@ -205,7 +205,7 @@ G4double
 GetHorizontalRes(G4double t,G4double y, G4double* par){
 	double p0 = par[0],p1 = par[1],p2 = par[2],p3=par[3],p4=par[4],p5=par[5];
 	double dl = y + 300;
-	double val = 1.2 * sqrt(
+	double val = 1. * sqrt(
 			p0*p0 + p2*p2*dl/(p3*exp(-p1*dl))
 			+p4*p4*tan(t)*tan(t)/(12*p5));
 	return val;
