9857852f102fdb13aa548ffa083043383eb6e78c
KANG Byung Min <kangbm@ccw01.cc.kek.jp>
2024-05-29 13:09:09 +0900

    Fixed bug on position smearing

--- src/ResHypTPC.cc
+++ src/ResHypTPC.cc
@@ -205,7 +205,7 @@ G4double
 GetHorizontalRes(G4double t,G4double y, G4double* par){
 	double p0 = par[0],p1 = par[1],p2 = par[2],p3=par[3],p4=par[4],p5=par[5];
 	double dl = y + 300;
-	double val = sqrt(
+	double val = 1.2 * sqrt(
 			p0*p0 + p2*p2*dl/(p3*exp(-p1*dl))
 			+p4*p4*tan(t)*tan(t)/(12*p5));
 	return val;
@@ -213,7 +213,7 @@ GetHorizontalRes(G4double t,G4double y, G4double* par){
 G4ThreeVector
 GetResVector(G4ThreeVector pos,G4ThreeVector mom, G4double* par_y, G4double* par_t){
 	G4ThreeVector XZ(1,0,1);
-	double PadAngle = atan2(-pos.x(),pos.z());
+	double PadAngle = atan2(-pos.x(),(pos.z()));
 	double TrackAngle = PadAngle - atan2(-mom.x(),mom.z());
 	double res_y = GetVerticalRes(pos.y(), par_y);
 	double res_t = GetHorizontalRes(TrackAngle,pos.y(),par_t);
@@ -226,12 +226,12 @@ GetSmearingVector(G4ThreeVector pos,G4ThreeVector mom, G4double* par_y, G4double
 	auto res_vect = GetResVector(pos,mom,par_y,par_t);
 	double res_y = res_vect.y();
 	double res_t = hypot(res_vect.x(),res_vect.z());
-	double PadAngle = atan2(pos.x(),pos.z());
+	double PadAngle = atan2(pos.x(),(pos.z()));
 	
 	double smear_y = CLHEP::RandGauss::shoot(0,res_y);
 	double smear_t = CLHEP::RandGauss::shoot(0,res_t);
-	double smear_x =abs( cos(PadAngle)) *smear_t; 	
-	double smear_z =abs( sin(PadAngle)) *smear_t; 	
+	double smear_x = cos(PadAngle) *smear_t; 	
+	double smear_z = sin(PadAngle) *smear_t; 	
 	return G4ThreeVector(smear_x,smear_y,smear_z);
 }
 
