b7a856453d2d8bac016f2a7aaa1d3553af647aa4
KANG Byung Min <kangbm@cw07.cc.kek.jp>
2024-12-02 15:23:20 +0900

    merge

--- submitKPUniform.sh
+++ submitKPUniform.sh
@@ -2,11 +2,11 @@
 #for i in {5723..5732}
 #for i in {5723..5756}
 #for i in {5177,5723}
-for i in {0..399}
+for i in {0..499}
 do
-  name="bsub -q l ./bin/Linux-g++/hyptpc1 param/conf/KpUniformCH2.conf rootfiles_ana/KpHists/KpUniformHist0${i}_Geant4.root g4macro/run_100k.mac"
+  name="bsub -q s ./bin/Linux-g++/hyptpc1 param/conf/KpUniformCH2.conf rootfiles_ana/KpHists/KpUniformHist0${i}_Geant4.root g4macro/run_100k.mac"
   $name
-  name="bsub -q l ./bin/Linux-g++/hyptpc1 param/conf/KpUniformCarbon.conf rootfiles_ana/KpHists/KpUniformHistCarbon0${i}_Geant4.root g4macro/run_100k.mac"
-  $name
+#  name="bsub -q l ./bin/Linux-g++/hyptpc1 param/conf/KpUniformCarbon.conf rootfiles_ana/KpHists/KpUniformHistCarbon0${i}_Geant4.root g4macro/run_100k.mac"
+#  $name
 done
 name=""
